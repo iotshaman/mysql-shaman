da0d87426e1c245a4f27e868bfc3c1d7442c14c0
Kyle Brown <kbrown.watco@gmail.com>
2020-11-16 10:38:40 -0600

    removing cli from ORM package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './cli/mysql-shaman-cli.config';
-export * from './cli/mysql-shaman-cli';
 export * from './collection';
 export * from './database.context';
 export * from './mysql.functions';
